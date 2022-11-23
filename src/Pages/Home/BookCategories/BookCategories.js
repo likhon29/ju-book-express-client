fdc18381875413a19c66ba86f08807bbe1b4a6b9
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2022-11-24 03:16:33 +0600

    load category name from database

--- src/Pages/Home/BookCategories/BookCategories.js
+++ src/Pages/Home/BookCategories/BookCategories.js
@@ -3,51 +3,61 @@ import CategoryCard from './CategoryCard';
 import clock from '../../../Assets/images/logo1.png';
 import marker from '../../../Assets/images/logo1.png';
 import phone from '../../../Assets/images/logo1.png';
+import { useQuery } from '@tanstack/react-query';
 const BookCategories = () => {
-    const categoriesData = [
-        {
-            id: 1,
-            name: 'Job Preparation Book',
-            // description: 'Open 9.00 am to 5.00pm everyday',
-            icon: clock,
-            bgClass: 'bg-gradient-to-r from-primary to-secondary'
-        },
-        {
-            id: 2,
-            name: 'Academic Books',
-            icon: marker,
-            bgClass: 'bg-accent'
-        },
-        {
-            id: 3,
-            name: 'Skill Development Book',
-            // description: 'Open 9.00 am to 5.00pm everyday',
-            icon: phone,
-            bgClass: 'bg-gradient-to-r from-primary to-secondary'
-        },
-        {
-            id: 4,
-            name: 'English Novel Book',
-            // description: 'Open 9.00 am to 5.00pm everyday',
-            icon: phone,
-            bgClass: 'bg-accent'
-        },
-        {
-            id: 5,
-            name: 'Science Fiction Book',
-            // description: 'Open 9.00 am to 5.00pm everyday',
-            icon: phone,
-            bgClass: 'bg-gradient-to-r from-primary to-secondary'
-        },
-        {
-            id: 6,
-            name: 'Admission Test Book',
-            // description: 'Open 9.00 am to 5.00pm everyday',
-            icon: phone,
-            bgClass: 'bg-accent'
-        },
+    // const categoriesData = [
+    //     {
+    //         id: 1,
+    //         name: 'Job Preparation Book',
+    //         // description: 'Open 9.00 am to 5.00pm everyday',
+    //         icon: clock,
+    //         bgClass: 'bg-gradient-to-r from-primary to-secondary'
+    //     },
+    //     {
+    //         id: 2,
+    //         name: 'Academic Books',
+    //         icon: marker,
+    //         bgClass: 'bg-accent'
+    //     },
+    //     {
+    //         id: 3,
+    //         name: 'Skill Development Book',
+    //         // description: 'Open 9.00 am to 5.00pm everyday',
+    //         icon: phone,
+    //         bgClass: 'bg-gradient-to-r from-primary to-secondary'
+    //     },
+    //     {
+    //         id: 4,
+    //         name: 'English Novel Book',
+    //         // description: 'Open 9.00 am to 5.00pm everyday',
+    //         icon: phone,
+    //         bgClass: 'bg-accent'
+    //     },
+    //     {
+    //         id: 5,
+    //         name: 'Science Fiction Book',
+    //         // description: 'Open 9.00 am to 5.00pm everyday',
+    //         icon: phone,
+    //         bgClass: 'bg-gradient-to-r from-primary to-secondary'
+    //     },
+    //     {
+    //         id: 6,
+    //         name: 'Admission Test Book',
+    //         // description: 'Open 9.00 am to 5.00pm everyday',
+    //         icon: phone,
+    //         bgClass: 'bg-accent'
+    //     },
 
-    ]
+    // ]
+
+    const {data: categoriesData =[] ,refetch, isLoading} = useQuery({
+        queryKey: ['categoriesData'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/categoryName');
+            const data = await res.json();
+            return data
+        }
+    })
 
     return (
         <div className="">
@@ -55,7 +65,7 @@ const BookCategories = () => {
             <div className='grid mt-8 gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
             {
                 categoriesData.map(card => <CategoryCard
-                    key={card.id}
+                    key={card._id}
                     card={card}
                 ></CategoryCard>)
             }
--- src/Pages/Home/BookCategories/CategoryCard.js
+++ src/Pages/Home/BookCategories/CategoryCard.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const CategoryCard = ({ card }) => {
-  const { id,name, description, icon, bgClass } = card;
+  const { _id,category_name } = card;
   return (
-    <div className={`card text-white p-4   md:card-side shadow-xl ${bgClass}`}>
-          <Link to={`/category/${id}`}>
-          <h2 className="card-title">{name}</h2>
+    <div className={`card text-white p-4   md:card-side shadow-xl bg-gradient-to-r from-primary to-secondary`}>
+          <Link to={`/category/${_id}`}>
+          <h2 className="card-title">{category_name}</h2>
       </Link>
     </div>
   );
